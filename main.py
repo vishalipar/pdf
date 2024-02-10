baa2a447e45580adbddb6534f12d5f960668f7b1
vishalipar <vishalipar04@gmail.com>
2024-02-10 14:20:39 +0530

    Add more pages for each topic.

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row['Topics'], align='L', ln=1)
     pdf.line(10,20,200,20)
 
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
+
 
 pdf.output('output.pdf')

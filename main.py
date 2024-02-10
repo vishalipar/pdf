eb4736bcefdc1438ef7de25bf03b1c2e44421bbd
vishalipar <vishalipar04@gmail.com>
2024-02-10 14:43:34 +0530

    Add footers.

--- main.py
+++ main.py
@@ -10,11 +10,22 @@ for index, row in df.iterrows():
 
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100,20,0)
-    pdf.cell(w=0, h=12, txt=row['Topics'], align='L', ln=1)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
     pdf.line(10,20,200,20)
+
+    #set the footer
+    pdf.ln(240)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+
+        pdf.ln(255)
+        pdf.set_font(family='Times', style='I', size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
 
 pdf.output('output.pdf')
